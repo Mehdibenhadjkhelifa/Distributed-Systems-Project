d1ff4f83c81009b05f4c069a0bc958eb48023b8b
Mehdi Ben Hadj Khelifa <mehdi.bhk13@gmail.com>
2023-12-30 12:06:14 +0100

    Added a setup script for ubuntu systems

---
+++ ubuntuSetup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+sudo apt install mysql server libmysqlclient-dev 
+
+sudo mysql<file.sql
+
+sudo service mysql restart
+#need to have root as mysql user and root as password
+#and have 'votre_table' as a table in your mysql database
